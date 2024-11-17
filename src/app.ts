b296479b29b2a0aec7b9e1a98a24973dce4da195
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2024-11-17 00:24:27 +0000

    Add 404 handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { delayMiddleware } from './middlewares/delay';
+import { createStatusResponse } from './models/statusResponse';
 import { indexRouter } from './routes/index';
 import { mirrorRouter } from './routes/mirror';
 import { statusRouter } from './routes/status';
@@ -25,6 +26,11 @@ app.use(express.json());
 app.use('/', indexRouter);
 app.use('/mirror', mirrorRouter);
 app.use('/status', statusRouter);
+
+// 404 handler
+app.use((_req, res) => {
+  res.status(404).json(createStatusResponse(404, 'Resource not found'));
+});
 
 app.listen(EnvConfig.PORT, () => {
   console.log(`Server is running on http://localhost:${EnvConfig.PORT}`);
