8c269506f433b873d29bf7aad8f8cd0c65f7ff9d
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2025-05-22 12:17:29 +0000

    Change mirror.ts processing

--- src/routes/mirror.ts
+++ src/routes/mirror.ts
@@ -3,12 +3,7 @@ import { Router } from 'express';
 const mirrorRouter = Router();
 
 mirrorRouter.all('/', (req, res) => {
-  res.json({
-    'method': req.method,
-    'query': req.query,
-    'headers': req.headers,
-    'body': req.method === 'GET' || req.body === undefined ? {} : req.body
-  });
+  res.json(req.method === 'GET' || req.body === undefined ? {} : req.body);
 });
 
 export { mirrorRouter };
