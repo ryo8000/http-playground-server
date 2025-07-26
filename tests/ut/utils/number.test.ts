491c40eded5c62f708bd77c2738880f4563dc925
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2025-07-26 15:08:04 +0000

    Update number.test.ts

--- tests/ut/utils/number.test.ts
+++ tests/ut/utils/number.test.ts
@@ -1,39 +1,39 @@
 import { toSafeInteger } from '../../../src/utils/number.js';
 
 describe('toSafeInteger', () => {
-  it('should return a number when given a valid integer string', () => {
-    expect(toSafeInteger('123')).toBe(123);
-    expect(toSafeInteger('-456')).toBe(-456);
-    expect(toSafeInteger('0')).toBe(0);
-    expect(toSafeInteger(Number.MAX_SAFE_INTEGER.toString())).toBe(9007199254740991);
-    expect(toSafeInteger(Number.MIN_SAFE_INTEGER.toString())).toBe(-9007199254740991);
+  describe('when given a valid integer string', () => {
+    const validIntegerTestCases = [
+      { value: '123', expected: 123 },
+      { value: '-456', expected: -456 },
+      { value: '0', expected: 0 },
+      { value: Number.MAX_SAFE_INTEGER.toString(), expected: 9007199254740991 },
+      { value: Number.MIN_SAFE_INTEGER.toString(), expected: -9007199254740991 },
+    ] as const;
+
+    it.each(validIntegerTestCases)(
+      'should return the expected integer for $value',
+      ({ value, expected }) => {
+        expect(toSafeInteger(value)).toBe(expected);
+      }
+    );
   });
 
-  it('should return undefined when given a non-integer string', () => {
-    const invalidInputs = [
-      undefined,
-      'abc',
-      '2e1',
-      '12.3',
-      ' 123 ',
-      '0123',
-      '',
-      ' ',
-    ];
+  describe('when given a non-integer value', () => {
+    const invalidInputs = [undefined, 'abc', '2e1', '12.3', ' 123 ', '0123', '', ' '] as const;
 
-    for (const input of invalidInputs) {
+    it.each(invalidInputs)('should return undefined for %s', (input) => {
       expect(toSafeInteger(input)).toBeUndefined();
-    }
+    });
   });
 
-  it('should return undefined when given an unsafe integer string', () => {
+  describe('when given an unsafe integer string', () => {
     const unsafeIntegerStrings = [
       (Number.MAX_SAFE_INTEGER + 1).toString(),
       (Number.MIN_SAFE_INTEGER - 1).toString(),
-    ];
+    ] as const;
 
-    for (const input of unsafeIntegerStrings) {
+    it.each(unsafeIntegerStrings)('should return undefined for %s', (input) => {
       expect(toSafeInteger(input)).toBeUndefined();
-    }
+    });
   });
 });
