84987c3c1af3e81ea625d20faf7a60094e1a5d88
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-06-27 14:43:51 +0900

    feat: remove unused configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,8 @@
-import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  js.configs.recommended,
   ...tseslint.configs.recommended,
-  prettier,
   {
     rules: {
       '@typescript-eslint/no-unused-vars': [
@@ -31,7 +28,8 @@ export default tseslint.config(
       'no-console': 'off',
     },
   },
+  eslintConfigPrettier,
   {
-    ignores: ['dist/', 'node_modules/', 'coverage/'],
+    ignores: ['dist/', 'coverage/'],
   }
 );
